f1778527b232ba8f4e2fa0125c64f1021ec8da11
KellySun <kellysun0124@gmail.com>
2024-02-19 13:27:34 -0600

    add commented out code for databse in server.js

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -6,7 +6,25 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => res.render('home'))
+app.get
 
+
+
+// THIS IS HOW WE wouLD CONNECT TO MYSQL DATABASE
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "next_dbs"
+// })
+//
+// CHECK IF CONNECTING TO DATABASE IS SUCCESSFULL
+// db.connect((err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('connected to db')
+// })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
