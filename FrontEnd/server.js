28947e93c05395a1839f54345c563aea7070feee
KellySun <kellysun0124@gmail.com>
2024-02-19 14:02:42 -0600

    add and change home.ejs and server.ejs to be better

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -8,7 +8,10 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('home'))
-app.get
+app.get('/user-home', (req, res) => res.render('user-home'))
+app.get('/sign-up', (req, res) => res.render('sign-up'))
+app.get('/setup', (req, res) => res.render('setup'))
+
 
 
 
