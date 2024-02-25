ccc06cff9262d65072590bb0e274a8fc53ee22fc
KayvonPaygon <kpaganmaisonet@gmail.com>
2024-02-25 00:14:57 -0600

    BE-5 finished all homepage CRUD routes

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -15,17 +15,178 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-router.get("/", async (req, res) => {
-    try {
+const readUsersImagesQuery = `
+    SELECT * FROM IMAGE
+    WHERE USER_ID = ?;
+`
 
-        const connection = await pool.getConnection()
-        const [rows] = await connection.query("SELECT * FROM IMAGE;")
-        connection.release()
-        res.json(rows)
+const createImageQuery = `
+    INSERT INTO IMAGE (USER_ID, FILE_LOCATION, DATE_TIME, IMAGE_LOCATION, IMAGE_NAME, SPECIES) 
+    VALUES (?, ?, ?, ?, ?, ?);
+`
+
+const updateImageNameQuery = `
+    UPDATE IMAGE
+    SET IMAGE_NAME = ?
+    WHERE USER_ID = ? AND IMAGE_ID = ?;
+`
+
+const updateImageLocationQuery = `
+    UPDATE IMAGE
+    SET IMAGE_LOCATION = ?
+    WHERE USER_ID = ? AND IMAGE_ID = ?;
+`
+
+const updateImageSeciesQuery = `
+    UPDATE IMAGE
+    SET SPECIES = ?
+    WHERE USER_ID = ? AND IMAGE_ID = ?;
+`
+
+// get all images from a user
+// -> homepage/username
+router.get("/:USER_ID", async (req, res) => {
+    const { USER_ID } = req.params;
+    
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query(readUsersImagesQuery, [USER_ID]);
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error("Error retrieving images: ", error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+});
+
+// create an image
+// -> homepage/username
+router.post("/:USER_ID", async (req, res) => {
+    const { USER_ID } = req.params;
+    const { FILE_LOCATION, DATE_TIME, IMAGE_LOCATION, IMAGE_NAME, SPECIES } = req.body;
+    
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            createImageQuery,
+            [USER_ID, FILE_LOCATION, DATE_TIME, IMAGE_LOCATION, IMAGE_NAME, SPECIES]
+        );
+        connection.release();
+        res.status(201).json({
+            message: "Image created successfully",
+            imageId: result.insertId
+        });
+    } catch (error) {
+        console.error("Error creating image: ", error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+});
+
+// update an images location
+// -> homepage/username/100000000016/IMAGE_LOCATION
+router.patch("/:USER_ID/:IMAGE_ID/IMAGE_LOCATION", async (req, res) => {
+    const { USER_ID, IMAGE_ID } = req.params;
+    const { IMAGE_LOCATION } = req.body;
+
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            updateImageLocationQuery,
+            [IMAGE_LOCATION, USER_ID, IMAGE_ID]
+        );
+        connection.release();
+
+        res.status(200).json({
+            message: "Image column updated successfully",
+            imageId: IMAGE_ID
+        });
+    } catch (error) {
+        console.error(`Error updating image location's value: `, error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+
+});
+
+// update an images name
+// -> homepage/username/100000000016/IMAGE_NAME
+router.patch("/:USER_ID/:IMAGE_ID/IMAGE_NAME", async (req, res) => {
+    const { USER_ID, IMAGE_ID } = req.params;
+    const { IMAGE_NAME } = req.body;
+
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            updateImageNameQuery,
+            [IMAGE_NAME, USER_ID, IMAGE_ID]
+        );
+        connection.release();
+
+        res.status(200).json({
+            message: "Image column updated successfully",
+            imageId: IMAGE_ID
+        });
+    } catch (error) {
+        console.error(`Error updating image name's value: `, error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+
+});
+
+// update an images species
+// -> homepage/username/100000000016/SPECIES
+router.patch("/:USER_ID/:IMAGE_ID/SPECIES", async (req, res) => {
+    const { USER_ID, IMAGE_ID } = req.params;
+    const { SPECIES } = req.body;
+
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            updateImageSeciesQuery,
+            [SPECIES, USER_ID, IMAGE_ID]
+        );
+        connection.release();
+
+        res.status(200).json({
+            message: "Image column updated successfully",
+            imageId: IMAGE_ID
+        });
+    } catch (error) {
+        console.error(`Error updating species' value: `, error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+
+});
+
+const deleteImageQuery = `
+    DELETE FROM IMAGE
+    WHERE USER_ID = ? AND IMAGE_ID = ?;
+`
+
+// delete image
+// -> homepage/username/100000000016
+router.delete("/:USER_ID/:IMAGE_ID", async (req, res) => {
+    const { USER_ID, IMAGE_ID } = req.params;
+
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            deleteImageQuery,
+            [USER_ID, IMAGE_ID]
+        );
+        connection.release();
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({ error: "Image not found "});
+        } else {
+            res.status(200).json({
+                message: "Image deleted successfully",
+                imageId: IMAGE_ID
+            });
+        }
 
     } catch (error) {
-        console.error("Error retrieving images: ", error)
-        res.status(500).json({ error: "Internal Server Error"})
+        console.error("Error deleting image: ", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
