d18645f1c807d63eae3e0f3604d2bb5fb8e10ae0
Rosa Wessel <rosawessel@Rosas-MacBook-Pro.local>
2024-03-18 13:28:00 -0500

    added user-birds and instructions routing pages

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { InstructionsComponent } from './components/instructions';
+import { UserBirdsComponent } from './components/user-birds/user-birds.component';
 
 
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent},
+  { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'instructions', component: InstructionsComponent},
+  { path: 'user-birds', component: UserBirdsComponent},
 
   //redirect to home
   { path: '**', redirectTo: ''}
