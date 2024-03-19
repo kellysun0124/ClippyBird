def4795765c07961557d3d9e3d2654c543191c61
KellySun <kellysun0124@gmail.com>
2024-03-19 16:21:51 -0500

    login and register page function done

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -7,4 +7,15 @@ import { Component, HostBinding } from '@angular/core';
 })
 export class LoginComponent {
   @HostBinding('attr.data-component-id') componentId = 'login';
+
+  username: string = '';
+  password: string = '';
+
+  submitLogin() {
+    // Handle login info submission
+    console.log('Username:', this.username);
+    console.log('Username:', this.username);
+
+    //send data to server and verify login info with backend
+  }
 }
