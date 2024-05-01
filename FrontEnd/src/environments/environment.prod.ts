823103f985896912abda554b30638276bace80a0
KayvonPaygon <kpaganmaisonet@gmail.com>
2024-04-30 22:21:34 -0500

    final ui changes

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://server-dot-clippybird2.ew.r.appspot.com',
+    apiUrl: 'https://clippyserver.uc.r.appspot.com',
   };
   
--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://clippyserver.uc.r.appspot.com',
   };
   
