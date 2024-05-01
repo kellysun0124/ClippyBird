b174910df8ebf5ef894dd7275b4a0da12e80847f
Kelly Sun <kellysun0124@gmail.com>
2024-04-30 20:31:53 -0500

    instruction guide

--- FrontEnd/src/app/components/login/login.component.spec.ts
+++ FrontEnd/src/app/components/login/login.component.spec.ts
@@ -1,29 +1,29 @@
-import { Component } from '@angular/core';
-import { UserService } from '../services/user.service';
+// import { Component } from '@angular/core';
+// import { UserService } from '../services/user.service';
 
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  errorMessage: string = '';
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent {
+//   username: string = '';
+//   password: string = '';
+//   errorMessage: string = '';
 
-  constructor(private userService: UserService) {}
+//   constructor(private userService: UserService) {}
 
-  submitLogin() {
-    this.userService.login(this.username, this.password)
-      .subscribe(
-        () => {
-          // Successful login, redirect or perform other actions
-        },
-        error => {
-          this.errorMessage = error.message;
-        }
-      );
-  }
-}
+//   submitLogin() {
+//     this.userService.login(this.username, this.password)
+//       .subscribe(
+//         () => {
+//           // Successful login, redirect or perform other actions
+//         },
+//         error => {
+//           this.errorMessage = error.message;
+//         }
+//       );
+//   }
+// }
 
