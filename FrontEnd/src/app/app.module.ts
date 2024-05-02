f0caef482fea1403c74c545aa02eb0e67b9a9ea8
Rosa Wessel <rosawessel@Rosas-MacBook-Pro.local>
2024-05-02 14:39:39 -0500

    logout

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { HeaderComponent } from './layouts/header/header.component';
@@ -11,12 +16,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { InstructionsComponent } from './components/instructions/instructions.component';
 import { UserBirdsComponent } from './components/user-birds/user-birds.component';
 import { HomeComponent } from './components/home/home.component';
-
-import { MatCardModule } from '@angular/material/card';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
 import { LogoutComponent } from './components/logout/logout.component';
+
+
 
 
 
